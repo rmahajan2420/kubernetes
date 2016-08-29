cb110f74b301b09c3e6f328b2a17835bd2a76159
root <root@ip-172-31-32-9.ec2.internal>
2016-08-29 11:48:18 +0000

    Addeing

--- final/script/dynamic.sh
+++ final/script/dynamic.sh
@@ -14,19 +14,8 @@ ECHO=`which echo`
 AWS=`which aws`
 env1=$1
 module=$2
-#image=$3
-#containers=$4
 $AWS s3 cp s3://fameplus-qa-private/docker/tag.txt .
 tag=`cat tag.txt`
-
-#usage(){
-#  echo -e "\033[1;4m$0: Usage error. Usage: \033[0m"
-#  echo -e "${0} ${LIGHTPURPLE_UL} Execute the script as SVC-WEPW user" 
-#  echo -e "${0} ${LIGHTGREEN} -f ${GREEN_UL}INPUTFILEPATH ${NOCOLOUR} ${LIGHTGREEN}-e ${GREEN_UL}ENVIORNMENT PARAMETER${NOCOLOUR}"
-#  echo -e "${LIGHTPURPLE_UL}INPUTFILEPATH${NOCOLOUR}${CYAN} is the full disk path of the ${RED_UL}tar.gz file" 
-#  echo -e "${LIGHTPURPLE_UL}ENVIORNMENT PARAMETER ${NOCOLOUR} ${CYAN} can have one of these DEV/SYS/UAT/PREPROD values ${NOCOLOUR}"
-#  exit 1
-#}
 
 
 if [ $# != 2 ]; then
@@ -35,15 +24,15 @@ exit
 fi
 
 filename=$env1"_"$module.yaml
-$ECHO  "Generating $env1_$module.yaml file"
+$ECHO  "Generating" $env1"-"$module.yaml
 
 #Checking if module  exists in the appWithNginx.txt
-grep -i "$module" /root/kube_files/kubernetes/final/script/appWithNginx.txt >> /dev/null
+grep -i "$module" $FILEPATH/appWithNginx.txt >> /dev/null
 
 if [ $? -ne 0 ]; then
-  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" template_1cont.yaml > $filename
+  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" $FILEPATH/template_1cont.yaml > $filename
 else
-  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" template_2cont.yaml > $filename
+  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" $FILEPATH/template_2cont.yaml > $filename
 fi
 
 $ECHO "Deploying Module $module on Env $env1 with Version $image"

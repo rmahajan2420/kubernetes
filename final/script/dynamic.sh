18524b5893fd67af493ede64e139bfdfc91316fe
root <root@ip-172-31-32-9.ec2.internal>
2016-08-29 07:51:06 +0000

    added

--- final/script/dynamic.sh
+++ final/script/dynamic.sh
@@ -1,6 +1,6 @@
 ################################################################################
 ### This is a deployment script ################################################
-### It will ask for parameters such as environment name, module #################
+### It will ask for parameters such as env1ironment name, module #################
 ### based on the inputs it will generate a yaml deployment file on the fly #####
 ### and then calls the kubectl api to do the deployment #########################
 #################################################################################
@@ -11,11 +11,12 @@ SED=`which sed`
 KUBECTL=`which kubectl`
 FILEPATH=/root/kube_files/kubernetes/final/script
 ECHO=`which echo`
-env=$1
+AWS=`which aws`
+env1=$1
 module=$2
 #image=$3
-containers=$4
-aws s3 cp s3://fameplus-qa-private/docker/tag.txt .
+#containers=$4
+$AWS s3 cp s3://fameplus-qa-private/docker/tag.txt .
 tag=`cat tag.txt`
 
 #usage(){
@@ -28,19 +29,22 @@ tag=`cat tag.txt`
 #}
 
 
-if [ $# != 4 ]; then
+if [ $# != 2 ]; then
 $ECHO "Please Enter ENV name, Module, Image and number of containers"
 exit
 fi
 
-filename=$env"_"$module.yaml
-$ECHO  "Generating $env_$module.yaml file"
+filename=$env1"_"$module.yaml
+$ECHO  "Generating $env1_$module.yaml file"
 
-if [ $containers = '1' ]; then
-  $SED -e "s/SERVER/$env/g; s/APP/$module/g; s/IMAGE/$image/g" template_1cont.yaml > $filename
-elif [ $containers = '2' ]; then
-  $SED -e "s/SERVER/$env/g; s/APP/$module/g; s/IMAGE/$image/g" template_2cont.yaml > $filename
+#Checking if module  exists in the appWithNginx.txt
+grep -i "$module" /root/kube_files/kubernetes/final/script/appWithNginx.txt >> /dev/null
+
+if [ $? -ne 0 ]; then
+  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" template_1cont.yaml > $filename
+else
+  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" template_2cont.yaml > $filename
 fi
 
-$ECHO "Deploying Module $module on Env $env with Version $image"
+$ECHO "Deploying Module $module on Env $env1 with Version $image"
 $KUBECTL apply -f $FILEPATH/$filename

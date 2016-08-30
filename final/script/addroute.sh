628eb323c4652ff8d2e3d445d5afc14c8da4334a
ricky <rakesh.mahajan@tothenew.com>
2016-08-30 16:11:01 +0000

    Addeing

--- final/script/addroute.sh
+++ final/script/addroute.sh
@@ -9,13 +9,13 @@ K8SMASTERID=$($AWS ec2 describe-instances --region 'us-east-1' --filters Name=ta
 
 
 #K8SMINIONID=$($AWS ec2 describe-instances --region 'us-east-1' --filters Name=tag:Name,Values=$K8SMINIONNAME --query 'Reservations[].Instances[].[InstanceId]' --output text | sed '$!N;s/\n/ /' | awk '{print $1}')
-K8SMINIONID=$($AWS ec2 describe-instances --region 'us-east-1' --filters Name=tag:Role,Values=$K8SMINIONNAME --query 'Reservations[].Instances[].[InstanceId]' --output text )
+K8SMINIONID=$($AWS ec2 describe-instances --region 'us-east-1' --filters Name=tag:Role,Values=$K8SMINIONNAME Name=instance-state-name,Values=running --query 'Reservations[].Instances[].[InstanceId]' --output text )
 
 #SUBNETID=$($AWS ec2 describe-instances --region 'us-east-1' --filters Name=tag:Role,Values=$K8SMINIONNAME --query 'Reservations[].Instances[].[SubnetId]' --output text | sed '$!N;s/\n/ /' | awk '{print $1}')
 
 SUBNET_IDs=($($AWS ec2 describe-instances --region 'us-east-1' --filters Name=tag:Role,Values=$K8SMINIONNAME --query 'Reservations[].Instances[].[SubnetId]' --output text))
 
-ROUTETABLEID=($($AWS ec2 describe-route-tables --region us-east-1 --query 'RouteTables[*].Associations[*].RouteTableId[]' --filters "Name=association.subnet-id,Values=${SUBNET_IDs[1]}" --output text))
+ROUTETABLEID=($($AWS ec2 describe-route-tables --region us-east-1 --query 'RouteTables[*].Associations[*].RouteTableId[]' --filters "Name=association.subnet-id,Values=${SUBNET_IDs[2]}" --output text))
 
 
 echo "Master" $K8SMASTERID
@@ -32,7 +32,7 @@ do
 echo $K8SMINIONID
         DESTINATION_BLOCK=$(/usr/local/bin/kubectl describe nodes/$i | grep PodCIDR | awk '{print $2}')
         K8SMINIONID=$(/usr/local/bin/kubectl describe nodes/$i | grep ExternalID | awk '{print $2}')
-        $AWS ec2 create-route --region 'us-east-1' --route-table-id $ROUTETABLEID --destination-cidr-block $DESTINATION_BLOCK --instance-id $K8SMINIONID
+        $AWS ec2 create-route --region 'us-east-1' --route-table-id ${ROUTETABLEID[1]} --destination-cidr-block $DESTINATION_BLOCK --instance-id $K8SMINIONID
 done
 # Master Entry
 #$AWS ec2 create-route --route-table-id $ROUTETABLEID --destination-cidr-block 20.246.0.0/16 --instance-id $K8SMASTERID
--- final/script/dynamic.sh
+++ final/script/dynamic.sh
@@ -14,8 +14,23 @@ ECHO=`which echo`
 AWS=`which aws`
 env1=$1
 module=$2
+if [ $env1 == qa ] 
+then 
 $AWS s3 cp s3://fameplus-qa-private/docker/tag.txt .
+elif [ $env1 == uat ]
+then
+$AWS s3 cp s3://fameplus-uat-2/tag.txt .
+else
+
+echo "Invalid Env Name. Please enter qa or uat "
+exit 1
+fi
+
+
+
+
 tag=`cat tag.txt`
+
 
 
 if [ $# != 2 ]; then
@@ -24,15 +39,15 @@ exit
 fi
 
 filename=$env1"_"$module.yaml
-$ECHO  "Generating" $env1"-"$module.yaml
+$ECHO  "Generating" $env1"_"$module.yaml
 
 #Checking if module  exists in the appWithNginx.txt
 grep -i "$module" $FILEPATH/appWithNginx.txt >> /dev/null
 
 if [ $? -ne 0 ]; then
-  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" $FILEPATH/template_1cont.yaml > $filename
+  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" $FILEPATH/template_1cont.yaml > $FILEPATH/$filename
 else
-  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" $FILEPATH/template_2cont.yaml > $filename
+  $SED -e "s/SERVER/$env1/g; s/APP/$module/g; s/IMAGE/$tag/g" $FILEPATH/template_2cont.yaml > $FILEPATH/$filename
 fi
 
 $ECHO "Deploying Module $module on Env $env1 with Version $image"
